45d7c1dfd440d4a48067d6209ed0711f5dc80bca
Ajay <ajaykumawat9904@gmail.com>
2024-09-20 16:15:08 +0530

    feat(middleware): add cors, cookieParser, fileUpload , urlencoded middleware

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,27 @@
 import { config } from 'dotenv';
-import express from 'express';
+import express, { urlencoded } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import fileUpload from 'express-fileupload';
 
 const app = express();
 
 config({
     path: './config/config.env'
 })
+
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: '/tmp/'
+}))
 
 export default app;
