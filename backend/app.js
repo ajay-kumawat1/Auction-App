63efa24af3cac3582d368fa080efd09e29f242ef
Ajay <ajaykumawat9904@gmail.com>
2024-09-20 15:59:18 +0530

    feat(cloudinary): Add cloudinary configuration

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,10 @@
+import { config } from 'dotenv';
 import express from 'express';
+
 const app = express();
+
+config({
+    path: './config/config.env'
+})
 
 export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,12 @@
 import app from "./app.js";
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
+import cloudinary from 'cloudinary';
+
+cloudinary.v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
